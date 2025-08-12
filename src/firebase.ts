d837eae592cfde03b2826bcc79d038453ea7db9c
Aron Schüler <aron.schueler@ambient.digital>
2025-08-12 13:10:44 +0200

    feat: add firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMRdv3_nyNmdQ1Kl1vVKEvwESwC6Pxi28",
+  authDomain: "tttracker-9b5ad.firebaseapp.com",
+  projectId: "tttracker-9b5ad",
+  storageBucket: "tttracker-9b5ad.firebasestorage.app",
+  messagingSenderId: "547891585614",
+  appId: "1:547891585614:web:2c0777c62d47589a65d91f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
